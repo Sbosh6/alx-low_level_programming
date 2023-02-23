1c0572e2178d957a7fdb7744a5c2003db2f651fe
Sbosh6 <sbonisongubane492@gmail.com>
2023-02-23 00:15:39 -0800

    Sign is the boss

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+* print_sign - A function that prints signs
+*
+*  @n: function parameter
+*
+* Return: 0, 1, -1
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+return (0);
+}
+else
+{
+_putchar('-');
+_putchar(',');
+_putchar(' ');
+return (-1);
+}
+
+
+
+
+}
+
+
+
+
+
