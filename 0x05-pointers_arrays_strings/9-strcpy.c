748b97b24f9c7acc7998b6510bf1734d7128b4b6
Sbosh6 <sbonisongubane492@gmail.com>
2023-02-28 20:11:44 -0800

    Add a string half print test function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcpy - prints every other charecter
+ * @dest : char is a parameter
+ * @src : char is a parameter
+ */
+char *_strcpy(char *dest, char *src) {
+int i = 0;
+
+while (src[i] != '\0') {
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return dest;
+}
+
