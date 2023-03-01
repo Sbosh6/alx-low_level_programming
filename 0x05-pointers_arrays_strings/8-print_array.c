bf228f26355ba5117b7b257083c922d3cd10b87c
Sbosh6 <sbonisongubane492@gmail.com>
2023-02-28 20:00:26 -0800

    Add a string half print test function

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - prints every other charecter
+ * @a : int* is a parameter
+ * @n : int is a parameter
+ */
+void print_array(int *a, int n)
+{
+for (int i = 0; i < n; i++) {
+printf("%d", a[i]);
+if (i != n - 1)
+printf(", ");
+}
+printf("\n");
+}
