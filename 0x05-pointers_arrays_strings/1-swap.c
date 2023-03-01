40ed9acefd5d421258a1b974a41647f3822deb73
Sbosh6 <sbonisongubane492@gmail.com>
2023-02-28 18:31:07 -0800

    Add a swap function

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 /**
- * swap - Swaps the values of two integers.
+ * swap_int - Swaps the values of two integers.
  * @a Pointer to the first integer.
  * @b Pointer to the second integer.
  */
-void swap(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 int temp = *a;
 *a = *b;
