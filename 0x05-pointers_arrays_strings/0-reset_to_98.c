11210ef4ba105d0d5d455417c8ce6f1047c969d0
Sbosh6 <sbonisongubane492@gmail.com>
2023-02-28 18:07:25 -0800

    Add a pointer function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
- * 
+ *
  */
-void reset_to_98(int *n) {
-    *n = 98;
+void reset_to_98(int *n) 
+{
+ *n = 98;
 }
